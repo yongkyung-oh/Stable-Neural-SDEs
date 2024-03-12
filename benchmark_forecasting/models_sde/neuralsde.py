f3e8af1f2a380593b2bec5c9776cf4d3071c2b63
YongKyung OH <yongkyung-oh@outlook.com>
2024-03-12 00:54:30 -0700

    Update neuralsde.py

--- benchmark_forecasting/models_sde/neuralsde.py
+++ benchmark_forecasting/models_sde/neuralsde.py
@@ -88,7 +88,77 @@ class NeuralSDE(torch.nn.Module):
         # Linear map and return
         pred_y = self.linear(z_t)
         return pred_y
+    
 
+class NeuralSDE_forecasting(torch.nn.Module):
+    def __init__(self, func, input_channels, hidden_channels, output_channels, initial=True):
+        super().__init__()
+        self.func = func
+        self.initial = initial
+        self.initial_network = torch.nn.Linear(input_channels, hidden_channels)
+        
+        # self.linear = torch.nn.Linear(hidden_channels, output_channels)
+        self.linear = torch.nn.Sequential(torch.nn.Linear(hidden_channels, hidden_channels),
+                                          torch.nn.BatchNorm1d(hidden_channels), torch.nn.ReLU(), torch.nn.Dropout(0.1),
+                                          torch.nn.Linear(hidden_channels, output_channels))    
+        
+    def forward(self, times, coeffs, final_index, z0=None, stream=False, **kwargs):
+        # control module
+        self.func.set_X(*coeffs, times)
+        
+        if z0 is None:
+            assert self.initial, "Was not expecting to be given no value of z0."
+            if isinstance(self.func, ContinuousRNNConverter):  # still an ugly hack
+                z0 = torch.zeros(*batch_dims, self.hidden_channels, dtype=coeff.dtype, device=coeff.device)
+            else:
+                z0 = self.initial_network(self.func.X.evaluate(times[0]))
+        else:
+            assert not self.initial, "Was expecting to be given a value of z0."
+            if isinstance(self.func, ContinuousRNNConverter):  # continuing adventures in ugly hacks
+                z0_extra = torch.zeros(*batch_dims, self.input_channels, dtype=z0.dtype, device=z0.device)
+                z0 = torch.cat([z0_extra, z0], dim=-1)
+        
+        # Figure out what times we need to solve for
+        if stream:
+            t = times
+        else:
+            # faff around to make sure that we're outputting at all the times we need for final_index.
+            sorted_final_index, inverse_final_index = final_index.unique(sorted=True, return_inverse=True)
+            if 0 in sorted_final_index:
+                sorted_final_index = sorted_final_index[1:]
+                final_index = inverse_final_index
+            else:
+                final_index = inverse_final_index + 1
+            if len(times) - 1 in sorted_final_index:
+                sorted_final_index = sorted_final_index[:-1]
+            t = torch.cat([times[0].unsqueeze(0), times[sorted_final_index], times[-1].unsqueeze(0)])
+        
+        # Switch default solver
+        if 'method' not in kwargs:
+            kwargs['method'] = 'euler' # use 'srk' for more accurate solution for SDE 
+        if kwargs['method'] == 'euler':
+            if 'options' not in kwargs:
+                kwargs['options'] = {}
+            options = kwargs['options']
+            if 'dt' not in options:
+                time_diffs = times[1:] - times[:-1]
+                options['dt'] = max(time_diffs.min().item(), 1e-3)
+                        
+        time_diffs = times[1:] - times[:-1]
+        dt = max(time_diffs.min().item(), 1e-3)
+                        
+        z_t = torchsde.sdeint(sde=self.func,
+                              y0=z0,
+                              ts=t,
+                              dt=dt,
+                              **kwargs)
+                               
+        for i in range(len(z_t.shape) - 2, 0, -1):
+            z_t = z_t.transpose(0, i)
+        input_time = z_t.shape[1]
+        pred_y = self.linear(z_t[:,input_time-self.output_time:,:])
+        return pred_y
+    
         
 class Diffusion_model(torch.nn.Module):
     def __init__(self, input_channels, hidden_channels, hidden_hidden_channels, num_hidden_layers, theta=1.0, sigma=1.0, input_option=0, noise_option=0):
